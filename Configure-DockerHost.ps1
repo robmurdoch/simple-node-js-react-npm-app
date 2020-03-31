bedab4af88859392c8f2e30eb12428f011ce325f
Rob <hacker1x78@OTAPRD176ops.onmicrosoft.com>
2020-03-30 20:04:30 -0400

    Build please

---
+++ Configure-DockerHost.ps1
@@ -0,0 +1,32 @@
+docker network create jenkins
+docker volume create jenkins-docker-certs
+docker volume create jenkins-data
+
+docker image pull docker:dind
+docker container run `
+    --name docker-in-docker-host `
+    --rm `
+    --detach `
+    --privileged `
+    --network jenkins `
+    --network-alias docker `
+    --env DOCKER_TLS_CERTDIR=/certs `
+    --volume jenkins-docker-certs:/certs/client `
+    --volume jenkins-data:/var/jenkins_home `
+    --publish 2376:2376 `
+docker:dind
+
+docker image pull jenkinsci/blueocean
+docker container run `
+    --name jenkins-blueocean `
+    --rm `
+    --detach `
+    --network jenkins `
+    --env DOCKER_HOST=tcp://docker:2376 `
+    --env DOCKER_CERT_PATH=/certs/client `
+    --env DOCKER_TLS_VERIFY=1 `
+    --volume jenkins-data:/var/jenkins_home `
+    --volume jenkins-docker-certs:/certs/client:ro `
+    --publish 8080:8080 `
+    --publish 50000:50000 `
+    jenkinsci/blueocean
